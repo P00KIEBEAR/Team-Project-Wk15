705bdbc068bac1594c0eb8a663eaa69289a316d3
pat paggi <patpaggi@gmail.com>
2021-01-31 12:42:54 -0700

    schedule

--- controllers/api/schedule-route.js
+++ controllers/api/schedule-route.js
@@ -3,56 +3,102 @@ const { Appointment, Timeblock, User } = require('../../models');
 //routes will use /api/schedule/ {route}
 
 router.post('/', async (req, res) => {
-  const { time, date, day, text, type, user_id } = req.body;
-  const appt = await Appointment.create({
-    Appointments_time: time,
-    Appointments_date: date,
-    Appointments_day: day, 
-    Appointments_text: text,
-    Appointments_type: type,
+  const {
+    Appointments_time,
+    Appointments_date,
+    Appointments_day,
+    Appointments_text,
+    Appointments_type,
     user_id,
-  });
-  res.json(appt);
+  } = req.body;
+  const appt = await Appointment.create({
+    Appointments_time: Appointments_time,
+    Appointments_date: Appointments_date,
+    Appointments_day: Appointments_day,
+    Appointments_text: Appointments_text,
+    Appointments_type: Appointments_type,
+    user_id,
+  })
+    .then(dbAppointmentData => res.json(dbAppointmentData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.put('/', async (req, res) => {
   //put route code here
-  res.send("this Schedule route was successful (put)");
+  res.send('this Schedule route was successful (put)');
 });
 
 //renders main scheduling page
-router.get('/', async (req, res) => {
-  res.render('scheduling');
-});
+router.get('/', (req, res) => {
+  console.log('======================');
+  Appointment.findAll({
+    // where: {
+    //   user_id: req.session.user_id,
+    // },
 
+    attributes: [
+      'id',
+      'Appointments_time',
+      'Appointments_date',
+      'Appointments_day',
+      'Appointments_text',
+      // 'user_id'
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username'],
+      },
+      Timeblock,
+    ],
+  })
+    .then(appointments => {
+      appointments = appointments.map(appointment =>
+        appointment.get({ plain: true })
+      );
+      res.render('scheduling', {
+        appointments,
+        loggedIn: true,
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 router.get('/all', async (req, res) => {
   const appointments = await Appointment.findAll({
-    include: [{
-      model: User
-    }]
-  })
-  res.json(appointments)
+    // include: [
+    //   {
+    //     model: User,
+    //   },
+    // ],
+  });
+  console.log(appointments[1].Appointments_time);
+  res.json(appointments);
 });
 
-router.get("/:user", async (req, res) => {
+router.get('/:user', async (req, res) => {
   const { user } = req.params;
   const { id } = await User.findOne({
     where: {
-      username: user
-    }
-  })
+      username: user,
+    },
+  });
   const appointments = await Appointment.findAll({
     where: {
-      user_id: id
-    }
-  })
-})
+      user_id: id,
+    },
+  });
+});
 
 router.delete('/', async (req, res) => {
   //delete route code here
-  res.send("this Schedule route was successful (delete)");
-
+  res.send('this Schedule route was successful (delete)');
 });
 
 module.exports = router;
