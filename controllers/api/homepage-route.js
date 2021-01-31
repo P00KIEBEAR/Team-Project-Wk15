16e74c88ba53f677c842965ef8d4d57df1bf61a1
pat paggi <patpaggi@gmail.com>
2021-01-31 13:57:55 -0700

    one

--- controllers/api/homepage-route.js
+++ controllers/api/homepage-route.js
@@ -8,32 +8,32 @@ router.get('/', (req, res) => {
 
     where: {
       user_id: req.session.user_id,
+
     },
-  },
     attributes: ['id',
-    'Appointments_time',
-    'Appointments_date',
-    'Appointments_day',
-    'Appointments_text',
-    'user_id',
-  ],
+      'Appointments_time',
+      'Appointments_date',
+      'Appointments_day',
+      'Appointments_text',
+      'user_id',
+    ],
     include:
     {
       model: User, attributes: ['username'],
     }, Timeblock,
-    ],	   
-  })
-  .then(appointments => {
-    appointments = appointments.map(appointment =>
-      appointment.get({ plain: true }));
-    // console.log(appointments)
-
-    res.render('homepage', {
-      appointments, loggedIn: true,
-
-    });
 
   })
+    .then(appointments => {
+      appointments = appointments.map(appointment =>
+        appointment.get({ plain: true }));
+      // console.log(appointments)
+
+      res.render('homepage', {
+        appointments, loggedIn: true,
+
+      });
+    })
+})
   .catch(err => {
     console.log(err);
     res.status(500).json(err);
