366f22316596d097330769c0020e1919d3af9fba
pat paggi <patpaggi@gmail.com>
2021-01-31 18:03:36 -0700

    appt

--- controllers/api/homepage-route.js
+++ controllers/api/homepage-route.js
@@ -10,6 +10,14 @@ router.get('/', (req, res) => {
     // where: {
     //   user_id: req.session.user_id,
     // },
+    attributes: [
+      'id',
+      'Appointments_time',
+      'Appointments_date',
+      'Appointments_day',
+      'Appointments_text',
+      // 'user_id'
+    ],
     include:
     {
       model: User,
